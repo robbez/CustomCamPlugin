dbf14d60bb578c4c0349b39b88b1d96e1d67facb
Robert Bezirganyan <RobertBezirganyan@gmail.com>
2015-01-10 00:12:09 -0800

    reset to original

--- src/android/CustomCam.java
+++ src/android/CustomCam.java
@@ -9,38 +9,34 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Context;
-import android.content.Intent;
-
 
 public class CustomCam extends CordovaPlugin {
 	public static final String TAG = "Custom Cam";
-	private CallbackContext mCallContext;
-	/**
-	* Constructor.
-	*/
-	public CustomCam() {}
-	/**
-	* Sets the context of the Command. This can then be used to do things like
-	* get file paths associated with the Activity.
-	*
-	* @param cordova The context of the main Activity.
-	* @param webView The CordovaWebView Cordova is running in.
-	*/
-	
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		super.initialize(cordova, webView);
-		Log.v(TAG,"Init CustomCam");
-	}
-
-
-
-	public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		mCallContext = callbackContext;
-		Context context = cordova.getActivity().getApplicationContext();
-		Intent mIntent = new Intent(context, CameraActivity.class);
-		cordova.startActivityForResult(this, mIntent, 0);
-		return true;
-	}
-
+/**
+* Constructor.
+*/
+public CustomCam() {}
+/**
+* Sets the context of the Command. This can then be used to do things like
+* get file paths associated with the Activity.
+*
+* @param cordova The context of the main Activity.
+* @param webView The CordovaWebView Cordova is running in.
+*/
+public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+super.initialize(cordova, webView);
+Log.v(TAG,"Init CustomCam");
+}
+public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+final int duration = Toast.LENGTH_SHORT;
+// Shows a toast
+Log.v(TAG,"CustomCam received:"+ action);
+cordova.getActivity().runOnUiThread(new Runnable() {
+public void run() {
+Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), action, duration);
+toast.show();
+}
+});
+return true;
+}
 }
